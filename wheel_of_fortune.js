d4f8b7b2f5c696f3362e48bcd79f0d119a2c2c64
Waterbead <shinywaterbead@gmail.com>
2019-04-02 21:54:59 -0500

    Wheel of Fortune Solution Uploaded

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -8,38 +8,43 @@
 
 // Set a secret word!
 
-var word = 'ANSWER'; // ALL CAPS
+var word = 'YAY'; // ALL CAPS
 
 // Ask player if they would like to guess a letter or guess the
 // solution.
-
 var isGuessingLetter = true; // set to false to guess solution
-
 // If they are guessing a letter, set the letter that they are
 // guessing.
 
 var letter = 'A'; // UPPERCASE
-
+var guess = 'YAY';
 // SCRIPT ////////////////////////////////////////////////////////////
 
 // If guessing a letter...
-
+if(isGuessingLetter){
+	if (letter === 'A'){
+		console.log("You got a letter!");
+		console.log("_A_");
+	} else if(letter === "Y") {
+		console.log("You got a letter!");
+		console.log("Y_Y"); 	
+	} else {
+	    console.log("Sorry, no" + letter + "!");
+	}	
+} else {
+	if(guess===word){
+		console.log("Congratulations! You won Wheel of Fortune!");
+	} else {
+		console.log("Nope, keep trying!");
+	}
+}
 	// If the word contains the letter...
-
 		// Print back the word with all instances of that letter revealed.
-
 	// If the word does not contain the letter...
-
 		// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
-
 // If guessing the word...
-
 	// If they are correct...
-
 		// Print "Congratulations! You won Wheel Of Fortune!"
-
 	// If they are incorrect...
-
 		// Print back "Nope, keep trying!"
-
 // End of script!
